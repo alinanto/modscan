c88b2f03208284053b817a95377e088e146d6b21
alinanto <alinanto5@gmail.com>
2024-04-04 15:32:57 +0530

    added project

---
+++ bin/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_modscan_FILE C:/Users/alina/projects/modscan/bin/modscan.exe)
+set(__QT_DEPLOY_TARGET_modscan_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
---
+++ bin/CMakeFiles/modscan.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "modscan_autogen/timestamp" "custom" "modscan_autogen/deps"
+  "C:/Users/alina/projects/modscan/src/main.cpp" "CMakeFiles/modscan.dir/main.cpp.obj" "gcc" "CMakeFiles/modscan.dir/main.cpp.obj.d"
+  "C:/Users/alina/projects/modscan/src/mainwindow.cpp" "CMakeFiles/modscan.dir/mainwindow.cpp.obj" "gcc" "CMakeFiles/modscan.dir/mainwindow.cpp.obj.d"
+  "C:/Users/alina/projects/modscan/bin/modscan_autogen/mocs_compilation.cpp" "CMakeFiles/modscan.dir/modscan_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/modscan.dir/modscan_autogen/mocs_compilation.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
